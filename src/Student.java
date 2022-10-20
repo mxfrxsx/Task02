f8efd2441091a7f83789484f522d2fd898c346bd
mxfrxsx <115561232+mxfrxsx@users.noreply.github.com>
2022-10-20 16:22:33 +0200

    getter und setter eingefügt

--- src/Student.java
+++ src/Student.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class Student {
 
+
     //Attributes
     String firstname;
     String lastname;
@@ -11,11 +12,37 @@ public class Student {
 
     public enum SortKey {FIRSTNAME, LASTNAME, STUDENT_ID, WEIGHT, BIRTHDAY}
 
-    Student(){
-        new Student(null, null, 0, 0, new Date(0));
+    Student() {
+        new Student(null, null, studentId, 0, new Date(0));
     }
-    Student(String cFristname, String cLastname, long cstudentID, double cWeight, Date cBirthday){
+
+    Student(String cFristname, String cLastname, long cStudentID, double cWeight, Date cBirthday) {
+        firstname = cFristname;
+        lastname = cLastname;
+        cStudentID = studentId;
+        weight = cWeight;
+        birthday = cBirthday;
+
 
     }
+
+    public String getFirstname() {return firstname;}
+
+    public void setFirstname(String firstname) {this.firstname = firstname;}
+
+    public String getLastname() {return lastname;}
+
+    public void setLastname(String lastname) {this.lastname = lastname;}
+
+    public String getName(){return firstname + " " + lastname;}
+    public long getStudentId() {return studentId;}
+
+    public double getWeight() {return weight;}
+
+    public void setWeight(double weight) {this.weight = weight;}
+
+    public Date getBirthday() {return birthday;}
+
+    public void setBirthday(Date birthday) {this.birthday = birthday;}
 
 }
