85cf0d07f1ecd871f3e543583cc7711dcf93928a
mxfrxsx <115561232+mxfrxsx@users.noreply.github.com>
2022-10-20 11:09:35 +0200

    Class Student hinzugefügt - Stand: bei Constructor

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Student {
+
+    //Attributes
+    String firstname;
+    String lastname;
+    long studentId;
+    double weight;
+    Date birthday;
+
+    public enum SortKey {FIRSTNAME, LASTNAME, STUDENT_ID, WEIGHT, BIRTHDAY}
+
+    Student(){
+        new Student(null, null, studentId, 0, new Date(0));
+    }
+    Student(String cFristname, String cLastname, long cstudentID, double cWeight, Date cBirthday){
+
+    }
+
+}
